3f2a1ac227063965e130f6d0ee8a99d2087c041e
bala2728 <dhanambala37@gmail.com>
2022-11-15 10:29:03 +0530

    task controller

--- controllers/task.js
+++ controllers/task.js
@@ -17,6 +17,20 @@ class TaskController {
         }
         return res.status(200).json({ success: true, data: output, message: "Task Listed !" });
     }
+
+    async findOne(req,res){
+        let listone = await Task.findOne({});
+        return res.status(200).json({success: true, data: listone,message: " Task listed "});
+     }
+     async update(req,res){
+        let update =  await Task.updateOne({_id:req.body._id},req.body);
+        return res.status(200).json({success: true, data: update, message: "task update"})
+        
+     }
+     async delete(req,res){
+        let remove = await Task.deleteOne(req.body._id)
+        return res.status(200).json({success: true, data: remove, message: "task delete"})
+     }
 }
 
 module.exports = TaskController
